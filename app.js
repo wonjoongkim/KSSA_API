52842bf91cef6926899536bce2c376c392d3fe94
wonjoongkim <kwj0904@nate.com>
2024-03-18 11:27:20 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// // app.js
+
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const usersRouter = require("./API/Users");
+
+// const port = 3001;
+
+// app.use(express.static("build"));
+// app.use(cors());
+
+// // API 라우터 등록
+// app.use("/api/users", usersRouter);
+
+// app.listen(port, () => console.log(`'KASS API' ON PORT ${port}`));
